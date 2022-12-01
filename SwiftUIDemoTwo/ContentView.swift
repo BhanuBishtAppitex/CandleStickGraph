7265dd54345b89c256e8c409659b536d26fd3603
Applios Studio <solutionsappsoft@gmail.com>
2022-12-01 18:10:44 +0530

    Initial Commit

---
+++ SwiftUIDemoTwo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDemoTwo
+//
+//  Created by wOOx Technology on 01/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDemoTwo/SwiftUIDemoTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoTwoApp.swift
+//  SwiftUIDemoTwo
+//
+//  Created by wOOx Technology on 01/12/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
